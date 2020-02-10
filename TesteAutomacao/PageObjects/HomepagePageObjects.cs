04cafc481c210c4f7bbc28055c336323deda5da5
Gabriel Marques Berto <gabrielm.berto@hotmail.com>
2020-02-09 23:30:41 -0300

    Acessar página do produto

---
+++ TesteAutomacao/PageObjects/HomepagePageObjects.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteAutomacao.PageObjects
+{
+    public static class HomepagePageObjects
+    {
+        public static By signInButton = By.ClassName("login");
+        public static By productContainer = By.ClassName("product-container");
+    }
+
+    public class HomePageMethods
+    {
+        IWebDriver webDriver;
+        public HomePageMethods(IWebDriver driver)
+        {
+            this.webDriver = driver;
+        }
+
+        public void SelectProduct(string productName)
+        {         
+            try
+            {                
+                webDriver.FindElement(By.PartialLinkText(productName)).Click();                
+            }
+            catch (Exception)
+            {
+
+                throw new Exception(string.Format("product not found"));
+            }
+            
+        }
+
+
+
+    }
+}
---
+++ TesteAutomacao/TestCases/Homepage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using TesteAutomacao.PageObjects;
+
+namespace HomePage
+{
+    class HomePage
+    {
+        IWebDriver Webdriver;
+
+        [SetUp]
+        public void DriverInitialization()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("disable-infobars");
+            Webdriver = new ChromeDriver(@"D:\Projetos\C#\TesteAutomacao\TesteAutomacao\WebDrivers", chromeOptions);
+            Webdriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Webdriver.Url = "http://automationpractice.com/";
+            Webdriver.Manage().Window.Maximize();
+            OpenQA.Selenium.Support.UI.WebDriverWait webDriverWait = new WebDriverWait(Webdriver, TimeSpan.FromSeconds(40));
+        }
+
+        [Test]
+        public void AcessProduct()
+        {
+            HomePageMethods homePageMethods = new HomePageMethods(Webdriver);
+            homePageMethods.SelectProduct("Faded Short Sleeve T-shirts");
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            Webdriver.Close();
+        }
+
+    }
+}
